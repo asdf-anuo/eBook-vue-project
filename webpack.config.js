f15b2eddede6d2dc9d61ea5ed59e87e001b6e00d
leich <leich@jurassic.com.cn>
2019-12-27 10:17:57 +0800

    添加vue代理,解决跨域

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const webpack = require("webpack");
-console.log("webpack");
 const path = require("path"); // node 自带的模块 无需安装 
 const htmlWebpackPlugin = require("html-webpack-plugin"); // 处理 HTML 文件
 const openBrowserWebpackPlugin = require("open-browser-webpack-plugin");
 const extractTextWebpackPlugin = require("extract-text-webpack-plugin"); // 抽离样式 
-// 模块化开发兼打包 
-// js  
-// png/svg/jpg/iconfont
-// less/css/scss
-// vue 单文件组件 
-
+const url = 'http://localhost:1810';
 
 module.exports = {
     entry: ["./src/main.js"], // 入口文件
@@ -72,12 +66,10 @@ module.exports = {
                                     return [
                                         require("cssgrace"), // CSS代码美化 
                                         require("autoprefixer"), // 自动补全
-                                        require('postcss-px2rem-exclude')(
-                                            {
-                                                remUnit:100,
-                                                exclude:/mint-ui/i,  // 排除 antd-mobile不需要进行 rem 转换 
-                                            }
-                                        ), // px 转 rem 
+                                        require('postcss-px2rem-exclude')({
+                                            remUnit: 100,
+                                            exclude: /mint-ui/i, // 排除 antd-mobile不需要进行 rem 转换 
+                                        }), // px 转 rem 
                                     ]
                                 }
                             }
@@ -99,12 +91,10 @@ module.exports = {
                                     return [
                                         require("cssgrace"), // CSS代码美化 
                                         require("autoprefixer"), // 自动补全
-                                        require('postcss-px2rem-exclude')(
-                                            {
-                                                remUnit:100,
-                                                exclude:/mint-ui/i,  // 排除 antd-mobile不需要进行 rem 转换 
-                                            }
-                                        ), // px 转 rem 
+                                        require('postcss-px2rem-exclude')({
+                                            remUnit: 100,
+                                            exclude: /mint-ui/i, // 排除 antd-mobile不需要进行 rem 转换 
+                                        }), // px 转 rem 
                                     ]
                                 }
                             }
@@ -123,15 +113,22 @@ module.exports = {
         compress: true,
         hot: true,
         inline: true,
-        // open:true,  // 自动打开浏览器
+        // open: true, // 自动打开浏览器
         host: "0.0.0.0",
         // host:"localhost",
         port: 3300,
         publicPath: "",
         historyApiFallback: true, // history mode ==> html5 API 
         disableHostCheck: true,
-        proxy: { // 代理  解决跨域 
-
+        proxy: {
+            // 代理  解决跨域 
+            "/vue": {
+                target: url,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/vue': '/vue'
+                }
+            }
         }
     },
 
@@ -150,6 +147,11 @@ module.exports = {
             filename: "css/app.[hash:8].css", // 抽离样式的文件名
             allChunks: true, // 全部数据 处理
             disable: false // 抽离 
+        }),
+
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
         })
     ]
 }
